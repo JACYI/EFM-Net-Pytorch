9ab704ebef1d08de6caaf0cad2daab1f571227e4
yiyonghao <yiyonghao@bupt.edu.cn>
2023-04-08 17:27:05 +0800

    Add introduction in README.md

--- test.sh
+++
@@ -1,1 +0,0 @@
-nohup python -m torch.distributed.launch --master_port 12350 --nproc_per_node=1 main.py --exp_name test --attentions 16 --epochs 120 --dataset FGSC-23 --output_dir logs/test --novel_loss 0.5 > test.txt &
